5580ae4facbda49ca7d57f6333ad91d18a59e27c
Daniel <riverocrespodaniel@gmail.com>
2026-05-18 01:33:59 -0400

    fix: forzar agregar wagmi-config.js

---
+++ Arte_Tienda_Cripto_Marketplace/art_marketplace/frontend/src/lib/wagmi-config.js
@@ -0,0 +1,16 @@
+import { http, createConfig, injected, walletConnect } from 'wagmi'
+import { arbitrumSepolia, arbitrum } from 'wagmi/chains'
+
+const projectId = 'b02910bd7a66d789cf2c18c1e9720e29'
+
+export const config = createConfig({
+  chains: [arbitrumSepolia, arbitrum],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+  ],
+  transports: {
+    [arbitrumSepolia.id]: http('https://sepolia-rollup.arbitrum.io/rpc'),
+    [arbitrum.id]: http('https://arb1.arbitrum.io/rpc'),
+  },
+})
